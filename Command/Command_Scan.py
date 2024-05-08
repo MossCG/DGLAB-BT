b8b5daa99fb183f1088a6f6e422d696205028419
MossCG <1292141077@qq>
2024-05-08 16:13:50 +0800

    complete dglab device filter

--- Command/Command_Scan.py
+++ Command/Command_Scan.py
@@ -1,4 +1,4 @@
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 
 import BasicIO
 
@@ -7,15 +7,15 @@ async def cmd_scan():
     BasicIO.output_command("msg", "正在搜索设备......")
     BasicIO.output_command("event", "scanStart")
     devices = await BleakScanner.discover()
-    list_device = []
     for device in devices:
-        name = device.name
-        if name is None:
+        if device.name is None:
             continue
-        if name.find("D-LAB") > -1:
-            BasicIO.output_command("msg", "发现郊狼设备：" + device.address + "/" + device.name)
-            list_device.append(device)
+        if device.name.find("D-LAB ESTIM01") > -1:
+            BasicIO.output_command("msg", "发现郊狼V2设备：" + device.address + "/" + device.name)
             BasicIO.output_command("event", "deviceFound " + device.address)
-        else:
-            BasicIO.output_command("msg", "发现设备：" + device.address + "/" + device.name)
+            continue
+        if device.name.find("47L121000") > -1:
+            BasicIO.output_command("msg", "发现郊狼V3设备：" + device.address + "/" + device.name)
+            BasicIO.output_command("event", "deviceFound " + device.address)
+        BasicIO.output_command("msg", "发现设备：" + device.address + "/" + device.name)
     BasicIO.output_command("event", "scanComplete")
